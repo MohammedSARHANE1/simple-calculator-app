6de8872ad6fc4e37023b3b98e8ed00a86f036c53
MohammedSARHANE1 <mohamedsarhane0@gmail.com>
2023-11-15 14:33:42 +0100

    Made new change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [result, setResult] = useState(0); 
  
   function plus(e) { 
+    e.preventDefault(); 
     setResult((result) => result + Number(inputRef.current.value)); 
   }; 
  
@@ -61,11 +62,11 @@ return Error();
           placeholder="Type a number" 
         /> 
         <button onClick={plus}>add</button> 
-        <button onClick={minus}>minus</button> 
-        <button onClick={times}>times</button>
+        <button onClick={minus}>subtract</button> 
+        <button onClick={times}>multiply</button>
         <button onClick={divide}>divide</button> 
-        <button onClick={resetInput}>resetInput</button> 
-        <button onClick={resetResult}>resetResult</button>  
+        <button onClick={resetInput}>reset input</button> 
+        <button onClick={resetResult}>reset result</button>  
        
       </form> 
     </div> 
